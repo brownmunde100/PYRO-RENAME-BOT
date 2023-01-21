119a62a8f058d7ba6aa5edea98eaffc119013a28
brownmunde100 <122628387+brownmunde100@users.noreply.github.com>
2023-01-21 10:08:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -21,5 +21,3 @@ START_PIC = os.environ.get("START_PIC", "")
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
 PORT = os.environ.get('PORT', '8080')
-
-START_MESSAGE = os.environ.get("START_MESSAGE", "")
